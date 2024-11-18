c7af38ef0f00c72c387a97420f900856d5a61a51
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-11-18 13:50:18 +0600

    create user

---
+++ PracticeMongoDB.js
@@ -0,0 +1,72 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+const router = express.Router();
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api/v1", router);
+
+
+
+// Connect to MongoDB
+mongoose.connect("mongodb://localhost:27017/TaskManager", { autoIndex:true })
+    .then(() => console.log("Connected to MongoDB"))
+    .catch(err => console.log("Failed to connect to MongoDB:", err));
+
+
+// MongoDB Schema and data model creating
+const InsertData = mongoose.Schema({
+    email: {type: String, unique: true, required: true},
+    password: {type: String, required: true},
+    name: {type: String, required: true},
+    age: {type: Number, required: true},
+    address: {type: String, required: true},
+}, {versionKey: false, timestamps: true});
+
+const DataModel = mongoose.model("mongoPractice", InsertData);
+
+
+// Product Schema
+
+const ProductDetails = mongoose.Schema({
+    name: {type: String, required: true},
+    description: {type: String, required: true},
+    price: {type: Number, required: true},
+}, {versionKey: false, timestamps: true});
+
+co
+
+
+// Data Controller for server request creating
+const Registration = async (req, res) => {
+    try{
+        let reqBody = req.body;
+        let result =  await DataModel.create(reqBody);
+        if (!result){
+            res.status(404).send({message:"Registration failed"});
+        }else {
+            res.json({status:"success", data: result});
+        }
+    }
+    catch(err){
+        res.status(400).json({status: "error", error: err});
+    }
+}
+
+const Product = async (req, res) => {
+
+}
+
+
+
+// making routes
+
+router.post("/register", Registration);
+
+
+
+app.listen(5522, () => console.log("Listening on port 5522"));
